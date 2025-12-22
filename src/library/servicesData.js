a1102ec568a532bba4c7f64d201561372231c815
lizan <lizansarkar16@gmail.com>
2025-12-22 21:39:42 +0600

    care web application folder structure and create needs route

---
+++ src/library/servicesData.js
@@ -0,0 +1,14 @@
+export const services = [
+ {
+  id: "baby",
+  name: "Baby Care",
+  price: 500,
+  description: "Professional baby sitting service"
+ },
+ {
+  id: "elderly",
+  name: "Elderly Care",
+  price: 700,
+  description: "Elderly home care service"
+ }
+]
